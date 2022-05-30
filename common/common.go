830328e5e44ce4e83de9cfef90796906a868c41f
이연지 <yzzeee@innogrid.com>
2022-05-30 19:42:28 +0900

    interface 구조체에서 lodash 처럼 쓸 수 있는 Get Util 추가 (gjson 있지만, 간단하게 사용하기 위함)

---
+++ common/common.go
@@ -0,0 +1,73 @@
+package common
+
+import (
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+// Get 두 개의 인자를 받아 두 번째 인자의 경로에 해당하는 값을 반환
+func Get(args ...interface{}) interface{} {
+	if len(args) < 2 {
+		panic("Invalid number of argument. At least 2 arguments are required")
+	}
+	var (
+		path     string
+		mapData  interface{}
+		fallback interface{}
+	)
+
+	if len(args) >= 1 {
+		mapData = args[0]
+	}
+	if len(args) >= 2 {
+		path = args[1].(string)
+	}
+	if len(args) >= 3 {
+		fallback = args[2]
+	}
+	if mapData == nil {
+		return fallback
+	}
+	defer func() interface{} {
+		if r := recover(); r != nil {
+			return fallback
+		}
+		return fallback
+	}()
+	data := mapData
+	paths := strings.Split(path, ".")
+	for _, key := range paths {
+		value := reflect.ValueOf(data)
+		dataType := value.Type().Kind()
+		if dataType == reflect.Map {
+			data = data.(map[string]interface{})[key]
+			continue
+		}
+
+		if dataType == reflect.Slice {
+			indx, err := strconv.Atoi(key)
+			if err != nil {
+				return fallback
+			}
+			data = value.Index(indx).Interface()
+			continue
+		}
+
+		if dataType == reflect.Struct {
+			data = value.FieldByName(key).Interface()
+			continue
+		}
+
+		if dataType == reflect.String {
+			indx, err := strconv.Atoi(key)
+			if err != nil {
+				return fallback
+			}
+			data = string(data.(string)[indx])
+			continue
+		}
+		return fallback
+	}
+	return data
+}
